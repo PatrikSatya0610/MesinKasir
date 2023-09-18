36456cf882e0965559c1ebe26f197896327c3330
Patrik Satya <pspr0621@gmail.com>
2023-09-18 14:15:33 +0700

    PROJECT

--- MesinKasir.java
+++ MesinKasir.java
@@ -11,20 +11,26 @@ public class MesinKasir{
         double pembayaran,total_harga,kembalian;
 
         //MENU
-        System.out.println(" ---------------------------------------");
-        System.out.println("|             LARAVEL COFFEE            |");
-        System.out.println("|                 MENU                  |");
-        System.out.println("|---------------------------------------|");
-        System.out.println("|COFFEE :                               |");
-        System.out.println("|   CAPPUCINO           : Rp 15.000     |");
-        System.out.println("|   COFFEE LATTE        : Rp 15.000     |");
-        System.out.println("|   VANILLA LATTE       : Rp 15.000     |");
-        System.out.println("|   HAZELNUT LATTE      : Rp 15.000     |");
-        System.out.println("|   STRAWBERY LATTE     : Rp 15.000     |");
-        System.out.println("|   AMERICANO           : Rp 15.000     |");
-        System.out.println("|   PICCOLO LATTE       : Rp 15.000     |");
-        System.out.println("|   CARAMEL MACHIATO    : Rp 15.000     |");
-        System.out.println("|   COFFEEMILK          : Rp 15.000     |");
+        System.out.println(" -------------------------------------------------------");
+        System.out.println("|                     LARAVEL COFFEE                    |");
+        System.out.println("|                          MENU                         |");
+        System.out.println("|-------------------------------------------------------|");
+        System.out.println("|COFFEE :                    HOT            ICE         |");
+        System.out.println("|   CAPPUCINO           : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   COFFEE LATTE        : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   VANILLA LATTE       : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   HAZELNUT LATTE      : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   STRAWBERY LATTE     : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   AMERICANO           : Rp 15.000     Rp 17.000       |");
+        System.out.println("|   PICCOLO LATTE       : Rp 15.000                     |");
+        System.out.println("|   CARAMEL MACHIATO    : Rp 20.000     Rp 22.000       |");
+        System.out.println("|   COFFEEMILK          :               Rp 20.000       |");
+        System.out.println("|                                                       |");
+        System.out.println("|MANUAL BREW :                                          |");
+        System.out.println("|   VIETNAM DRIP        : Rp 22.000                     |");
+        System.out.println("|   V60                 : Rp 22.000                     |");
+        System.out.println("|   JAPANESE            : Rp 22.000                     |");
+        System.out.println("|-------------------------------------------------------|");
 
         // INPUT DATA
          System.out.println("Masukkan nama barang : ");
