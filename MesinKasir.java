287a213d8f220ebad471fc09b6ba95b7ed9f29e9
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-11-20 21:28:15 +0700

    Perbaikan pilihan Hot/Ice dengan menggunakan Array

--- MesinKasir.java
+++ MesinKasir.java
@@ -2,48 +2,48 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class MesinKasir{
+public class MesinKasir {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         LocalDate date = LocalDate.now();
 
         // VARIABEL DAN TIPE DATA UTAMA
-        String nama_barang = "", pelanggan, catatan="",konfirmasi,voucher;
-        int jml_barang=0, harga_barang=0,pilih;
-        double total_harga=0,kembalian=0,pajak1,pajak2,pembayaran=0, diskon=0;
-        pajak1= 0.05;
-        pajak2= 0.01;
+        String nama_barang = "", pelanggan, catatan = "", konfirmasi, voucher;
+        int jml_barang = 0, harga_barang = 0, pilih;
+        double total_harga = 0, kembalian = 0, pajak1, pajak2, pembayaran = 0, diskon = 0;
+        pajak1 = 0.05;
+        pajak2 = 0.01;
 
         // VARIABEL DAN TIPE DATA MENU
-        String menu= "";
-        int menu_item ;
-        String suhu="";
-        int input_menu=0;
-        int harga_menu[] ={20000,22000,17000,15000};
-        
+        String menu = "";
+        int menu_item;
+        String[] suhu = {"Hot","Ice"};
+        int input_menu = 0;
+        int harga_menu[] = { 20000, 22000, 17000, 15000 };
+
         // VARIABEL DAN TIPE DATA RESERVASI
         String nama_pelanggan, nomor_kontak, nama_pegawai;
         int jumlah_tamu = 0;
-        int[] nomor_meja = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+        int[] nomor_meja = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
 
         String username, password, secretUsername = "admin", secretPassword = "123456";
         for (int Try = 1; Try <= 3; Try++) {
-             System.out.print("Enter Username : ");
+            System.out.print("Enter Username : ");
             username = sc.nextLine();
             System.out.print("Enter Password : ");
             password = sc.nextLine();
             if (username.equals(secretUsername) && password.equals(secretPassword)) {
                 System.out.println("Login sukses");
                 break;
-            }else if (Try == 3){
+            } else if (Try == 3) {
                 System.out.println("Maaf Silahkan Coba Lagi Nanti");
                 System.exit(0);
-            }else if (Try <= 3){
+            } else if (Try <= 3) {
                 System.out.println("Login Gagal");
             }
         }
-           
-        //DISPLAY MENU
+
+        // DISPLAY MENU
         System.out.println(" ------------------------------------------------------- ");
         System.out.println("|                     LARAVEL COFFEE                    |");
         System.out.println("|                          MENU                         |");
@@ -67,7 +67,7 @@ public class MesinKasir{
 
         // INPUT DATA
         int i = 1;
-        do{
+        do {
             System.out.println("---------- Pilih Fitur ----------");
             System.out.println("| 1. Menu                       |");
             System.out.println("| 2. Reservasi                  |");
@@ -75,58 +75,52 @@ public class MesinKasir{
             System.out.println("|                               |");
             System.out.println("---------------------------------");
             konfirmasi = sc.next();
-           
+
             if (konfirmasi.equalsIgnoreCase("1")) {
-            
-                // System.out.print("Masukkan nama barang :");
-                // nama_barang = sc.next();
-                // System.out.print("Masukkan catatan pelanggan :");
-                // catatan = sc.next();
-                // sc.next();
-                // System.out.print("Masukkan harga per item : ");
-                // harga_barang = sc.nextInt();
-                // System.out.print("Masukkan jumlah barang yang dibeli :");
-                // jml_barang = sc.nextInt();
-                // System.out.println("");
-                for (String a = "Y"; a.equals("Y")||a.equals("y"); )
-                {
-                    System.out.print ("Masukkan Nomor Pesanan : ");
+
+                for (String a = "Y"; a.equals("Y") || a.equals("y");) {
+                    System.out.print("Masukkan Nomor Pesanan : ");
                     menu_item = sc.nextInt();
-        
+
                     System.out.print("Masukkan Banyak Pesanan : ");
                     jml_barang = sc.nextInt();
-                    if (menu_item == 1)
-                    {
+                    if (menu_item == 1) {
                         menu = "Cappucino";
                         System.out.println("Ingin Hot / Ice?");
-                        suhu = sc.next();
-                        if (suhu.equalsIgnoreCase("hot")) {
+                        System.out.println("1. Hot\n" + "2. Ice\n");
+                        System.out.print("Pilih : ");
+                        int iSuhu = sc.nextInt();
+                        if (suhu[iSuhu-1].equalsIgnoreCase("hot")) {
                             System.out.println("Cappucino Hot");
-                            total_harga= harga_menu[0] * jml_barang;
-                        }else if (suhu.equalsIgnoreCase("ice")){
+                            total_harga = harga_menu[0] * jml_barang;
+                        } else if (suhu[iSuhu-1].equalsIgnoreCase("ice")) {
                             System.out.println("Cappucino Ice");
-                            total_harga= harga_menu[1] * jml_barang;
+                            total_harga = harga_menu[1] * jml_barang;
                         }
-                        System.out.println("Menu Pesanan Anda : " + menu +"    x"+ jml_barang+"   |  Rp. "+ total_harga);
-                    }else if(menu_item == 2){
+                        System.out.println(
+                                "Menu Pesanan Anda : " + menu +" "+suhu[iSuhu-1]+"    x" + jml_barang + "   |  Rp. " + total_harga);
+                    } else if (menu_item == 2) {
                         menu = "Coffe Latte";
                         System.out.println("Ingin Hot / Ice?");
-                        suhu = sc.next();
-                        if (suhu.equalsIgnoreCase("hot")) {
+                        System.out.println("1. Hot\n" + "2. Ice\n");
+                        System.out.print("Pilih : ");
+                        int iSuhu = sc.nextInt();
+                        if (suhu[iSuhu-1].equalsIgnoreCase("Hot")) {
                             System.out.println("Coffee Latte Hot");
-                            total_harga= harga_menu[0] * jml_barang;
-                        }else if (suhu.equalsIgnoreCase("ice")){
+                            total_harga = harga_menu[0] * jml_barang;
+                        } else if (suhu[iSuhu-1].equalsIgnoreCase("Ice")) {
                             System.out.println("Coffee latte Ice");
-                            total_harga= harga_menu[1] * jml_barang;
+                            total_harga = harga_menu[1] * jml_barang;
                         }
-                        System.out.println("Menu Pesanan Anda : " + menu +"    x"+ jml_barang+"   |  Rp. "+ total_harga);
-                    }else{
+                        System.out.println(
+                                "Menu Pesanan Anda : " + menu +" "+suhu[iSuhu-1]+ "    x" + jml_barang + "   |  Rp. " + total_harga);
+                    } else {
                         System.out.println("Maaf Menu Yang Anda Pilih Tidak Tersedia.");
                     }
-                        System.out.println("Apakah anda mau memesan lagi? Y/T");
-                        a = sc.next();
+                    System.out.println("Apakah anda mau memesan lagi? Y/T");
+                    a = sc.next();
                 }
-            }else if(konfirmasi.equalsIgnoreCase("2")){
+            } else if (konfirmasi.equalsIgnoreCase("2")) {
 
                 System.out.print("Nama Kasir\t\t: ");
                 nama_pegawai = sc.nextLine();
@@ -134,7 +128,7 @@ public class MesinKasir{
                 nama_pelanggan = sc.nextLine();
                 System.out.print("Nomor Pelanggan\t\t: ");
                 nomor_kontak = sc.nextLine();
-        
+
                 System.out.println(" ------------------------------------------------------- ");
                 System.out.println("|_______________________________________________________|");
                 System.out.println("|                    MEJA RESERVASI                     |");
@@ -147,66 +141,66 @@ public class MesinKasir{
                 System.out.println("|_______________________________________________________|");
                 System.out.println(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
                 System.out.print("Pilih Nomor Meja :");
-        
+
                 int pilihMeja = sc.nextInt();
-        
-                // memeriksa ketersediaan meja 
+
+                // memeriksa ketersediaan meja
                 if (pilihMeja >= 1 && pilihMeja <= nomor_meja.length) {
                     System.out.println("Meja " + pilihMeja);
                 } else {
                     System.out.println("Silahkan Pilih Nomor Meja Yang Tersedia");
                 }
-        
+
                 System.out.print("Jumlah Pelanggan\t: ");
                 jumlah_tamu = sc.nextInt();
-                if (jumlah_tamu<= 5) {
-                    System.out.println("Jumlah Tamu Yang akan duduk :"+jumlah_tamu);      
-                }else{
+                if (jumlah_tamu <= 5) {
+                    System.out.println("Jumlah Tamu Yang akan duduk :" + jumlah_tamu);
+                } else {
                     System.out.println("Mohon Maaf Kapasistas Tempat duduk Terlalu banyak");
                 }
-            
+
                 System.out.println();
                 System.out.println("---- BUKTI RESERVASI ----");
-                System.out.println("Nama Pelanggan\t\t: "+nama_pelanggan);
-                System.out.println("Nomor Pelanggan\t\t: "+nomor_kontak);
-                System.out.println("Nomor Meja\t\t: "+ pilihMeja);
-                System.out.println("Jumlah Pelanggan\t: "+jumlah_tamu);
+                System.out.println("Nama Pelanggan\t\t: " + nama_pelanggan);
+                System.out.println("Nomor Pelanggan\t\t: " + nomor_kontak);
+                System.out.println("Nomor Meja\t\t: " + pilihMeja);
+                System.out.println("Jumlah Pelanggan\t: " + jumlah_tamu);
                 System.out.println("Tanggal Reservasi\t: " + date);
-                System.out.print("Nama Kasir\t\t: "+nama_pegawai);
-            }else if(konfirmasi.equalsIgnoreCase("9")){
+                System.out.print("Nama Kasir\t\t: " + nama_pegawai);
+            } else if (konfirmasi.equalsIgnoreCase("9")) {
                 break;
             }
-            
-            total_harga = harga_barang * jml_barang;
-        }while (i>0) ;
 
-        //  PROSES DATA
+            total_harga = harga_barang * jml_barang;
+        } while (i > 0);
+
+        // PROSES DATA
 
         // INPUT DATA STRUK PEMBAYARAN
-         System.out.print("Masukkan nama pelanggan :");
-         pelanggan = sc.next();
+        System.out.print("Masukkan nama pelanggan :");
+        pelanggan = sc.next();
 
-        //  PROSES DATA STRUK PEMBAYARAN
+        // PROSES DATA STRUK PEMBAYARAN
         total_harga = harga_barang * jml_barang;
         System.out.println("Total Harga : " + total_harga);
 
-         // PROSES DATA VOUCHER
+        // PROSES DATA VOUCHER
         System.out.println("Masukkan Kode Voucher : ");
         voucher = sc.next();
-            if (voucher.equals("LRVL03")){
-                if (total_harga >= 100000){
-                    diskon = 0.25;
-                    System.out.println("Selamat Anda mendapatkan potongan harga sebesar 25%");
-                }else 
-                    diskon = 0.1;
-                    System.out.println("Selamat Anda mendapatkan potongan harga sebesar 10%");
-            }else{
-                System.out.println("Kode Voucher Tidak Tersedia");
-            }
+        if (voucher.equals("LRVL03")) {
+            if (total_harga >= 100000) {
+                diskon = 0.25;
+                System.out.println("Selamat Anda mendapatkan potongan harga sebesar 25%");
+            } else
+                diskon = 0.1;
+            System.out.println("Selamat Anda mendapatkan potongan harga sebesar 10%");
+        } else {
+            System.out.println("Kode Voucher Tidak Tersedia");
+        }
 
         total_harga = total_harga - (total_harga * diskon);
         System.out.print("Total Harga = " + total_harga);
-       
+
         // PROSES DATA JENIS PEMBAYARAN
         System.out.println("\n\n*****************************");
         System.out.println("---------------------------------");
@@ -220,7 +214,7 @@ public class MesinKasir{
         pilih = sc.nextInt();
 
         switch (pilih) {
-            // TUNAI 
+            // TUNAI
             case 1:
                 System.out.print("Masukkan jumlah yang dibayar : ");
                 pembayaran = sc.nextDouble();
@@ -228,24 +222,24 @@ public class MesinKasir{
                 kembalian = pembayaran - total_harga;
                 break;
             // DEBIT
-            case 2 :
+            case 2:
                 System.out.print("Masukkan jumlah yang dibayar : ");
                 pembayaran = sc.nextDouble();
-                total_harga = (total_harga * pajak1)+total_harga;
+                total_harga = (total_harga * pajak1) + total_harga;
                 kembalian = pembayaran - total_harga;
                 break;
             // E-Money
-            case 3 :
+            case 3:
                 System.out.print("Masukkan jumlah yang dibayar : ");
                 pembayaran = sc.nextDouble();
-                total_harga =(total_harga * pajak2)+total_harga;
+                total_harga = (total_harga * pajak2) + total_harga;
                 kembalian = pembayaran - total_harga;
                 break;
             default:
                 System.out.println("Maaf, Silahkan pilih angka yang sesuai.");
                 break;
         }
-         
+
         // OUTPUT STRUK PEMBAYARAN
         System.out.println("");
         System.out.println("\n\n---- STRUK PEMBELIAN ----");
@@ -258,10 +252,9 @@ public class MesinKasir{
         System.out.println("Kembalian\t: " + kembalian);
         System.out.println("\n---- TERIMA KASIH ----");
 
- 
     }
-    
+
 }
-       // Tambahan note atau request
-        // Diskon & Voucher
-        // Biaya Charge/Penalty
+// Tambahan note atau request
+// Diskon & Voucher
+// Biaya Charge/Penalty
